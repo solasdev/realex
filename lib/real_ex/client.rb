75a07b8cd2441979185c65c0fa10fbac0f041d69
solasdev <mixmastermurf@gmail.com>
2014-09-22 13:56:39 +0100

    change ip back

--- lib/real_ex/client.rb
+++ lib/real_ex/client.rb
@@ -19,7 +19,7 @@ module RealEx
       end
     
       def call(url,xml)
-        h = Net::HTTP.new('epage.payandshop.com', 443, '54.72.12.1', 80)
+        h = Net::HTTP.new('epage.payandshop.com', 443, '54.75.227.164', 80)
         h.use_ssl = true
         response = h.request_post(url, xml)
         result = Nokogiri.XML(response.body)
